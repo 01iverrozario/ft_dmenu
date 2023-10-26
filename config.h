870d642631a86b4da1385abdcbd8493cc5b2c5a5
Oliver Bernard Rozario <busycake@outlook.com>
2023-10-26 14:33:03 +0000

    Changes to be committed: 	modified:   config.h  Changes not staged for commit: 	modified:   config.def.h

--- config.h
+++ config.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"ubuntu:size=10"
+	"UbuntuMono Nerd Font:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ffffff", "#222222" },
-	[SchemeSel] = { "#ffffff", "#f70000" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#ff1100" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
